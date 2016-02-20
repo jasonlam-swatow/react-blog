355b761dbed46a75f07b6923d7eb74a1e5f4785c
Jason Lam <jasonlam0619@yahoo.com>
2016-02-20 16:17:54 +0800

    Add PropTypes

--- src/scripts/AddPostFormComponent.jsx
+++ src/scripts/AddPostFormComponent.jsx
@@ -1,46 +1,32 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import helper from './helper.js';
 
-export default class AddPostForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.createPost = this.createPost.bind(this); // with es2015 classes in React, there is no autobinding.
-  }
-
-  createPost(event) {
-    event.preventDefault();
-    let post = {
-      title : this.refs.title.value,
-      name : this.refs.name.value,
-      desc : this.refs.desc.value,
-      image : this.refs.image.value
-    }
-    this.props.addPost(post);
-    this.refs.postForm.reset();
-  }
-
+export default class NewPost extends Component {
   render() {
+    let details = this.props.details;
     return (
-      <div className="callout secondary form-area">
-        <h5>Add a Post to the React Blog</h5>
-        <form className="post-edit" ref="postForm" onSubmit={this.createPost}>
-          <label> Post Title
-            <input type="text" ref="title" placeholder="Post Title" required/>
-          </label>
-          <label> Author Name
-            <input type="text" ref="name" placeholder="Full Name required" required/>
-          </label>
-          <label> Post Content
-          <textarea
-            ref="desc"
-            placeholder="Write your post here" required/>
-          </label>
-          <label> Image URL - <span className="highlight">use this one to test 'http://goo.gl/ETzSWv'</span>
-            <input type="url" ref="image" placeholder="The URL of the featured image for your post" required/>
-          </label>
-          <button type="submit" className="button">Add Post</button>
-        </form>
+      <div className="blog-post">
+        <h3 className="ptitle">
+          {details.title}
+          <small>{helper.getTime()}</small>
+        </h3>
+        <img className="thumbnail"
+          src={details.image}
+          alt={details.name} />
+        <p>{details.desc}</p>
+        <div className="callout callout-post">
+          <ul className="menu simple">
+            <li><a href="#">Author: {details.name}</a></li>
+            <li><a href="#">Comments: 0</a></li>
+            <li><a href="#">Tags: {helper.getTaggedName()}</a></li>
+          </ul>
+        </div>
       </div>
     )
   }
-}
+};
+
+NewPost.propTypes = {
+  details: PropTypes.object.isRequired
+};
